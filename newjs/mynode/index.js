a156113c26053666d9e1959fadef7e82ad809d8c
do4best657 <panic_4_what@hotmail.com>
2025-10-16 12:31:56 +0500

    POST and GET Methods are on the run in nodeJS

--- newjs/mynode/index.js
+++ newjs/mynode/index.js
@@ -1,10 +1,34 @@
 import http from 'http'
-const PORT = 8000;
-
+const port=process.env.PORT;
 const server = http.createServer((req,res)=>{
-    res.write("Hello Friend")
-    res.end()
-});
-server.listen(PORT,()=>{
-    console.log(`Server Running on ${PORT}`)
+   try{
+if(req.method === 'GET'){
+    if(req.url === '/'){
+    res.writeHead(400,{'content-type':'text/html'})
+   res.end("<h1>Hello Friend</h1>")
+   
+   }else if(req.url === '/about'){
+        res.writeHead(400,{'content-type':'text/html'})
+   res.end("<h1>This is About Page</h1>")
+   
+    }else {
+        res.writeHead(404,{'content-type':'text/html'})
+   res.end("<h1>No Page Found</h1>")
+   
+    }
+} else {
+    throw new Error('Nothing Found')
+}
+   }catch(error){
+   res.writeHead(500,{'content-type':'text/plain'})
+   res.end("Internal Error")
+   
+   }
+   })
+   
+
+
+
+server.listen(port,()=>{
+    console.log(`The server is running on Port ${port}`)
 })
