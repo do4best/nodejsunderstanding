a98c199e37438d650af54413e70f9c89069b752c
do4best657 <panic_4_what@hotmail.com>
2025-10-18 19:24:41 +0500

    Tried to work with some express thing

--- newjs/newexpress/index.js
+++ newjs/newexpress/index.js
@@ -3,10 +3,21 @@ import path from 'path'
 import  url  from 'url'
 const __fileName = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__fileName)
+const users = [
+    {id:1,name:"do4best"},
+    {id:2,name:"do4best2"},
+    {id:3,name:"do4best3"},
 
+]
 const PORT = process.env.PORT ||3000
 const app = express()
 app.get('/',(req,res)=>{
 res.sendFile(path.join(__dirname,'public','home.html'))
 })
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','about.html'))
+})
+ app.get('/api/user',(req,res)=>{
+    res.json(users)
+ })
 app.listen(PORT,()=>console.log(`Server is Running on ${PORT}`))
