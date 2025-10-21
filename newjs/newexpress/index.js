4a415758251acffa66feac215ca1725fa3b5e156
do4best657 <panic_4_what@hotmail.com>
2025-10-21 17:37:42 +0500

    Express Js exercised is being done

--- newjs/newexpress/index.js
+++ newjs/newexpress/index.js
@@ -9,6 +9,12 @@ const users = [
     {id:3,name:"do4best3"},
 
 ]
+const posts = [
+    {id:1,name:"do4best"},
+    {id:2,name:"do4best2"},
+    {id:3,name:"do4best3"},
+
+]
 const PORT = process.env.PORT ||3000
 const app = express()
 app.get('/',(req,res)=>{
@@ -17,7 +23,20 @@ res.sendFile(path.join(__dirname,'public','home.html'))
 app.get('/about',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','about.html'))
 })
- app.get('/api/user',(req,res)=>{
+
+
+ app.get('/api/posts/',(req,res)=>{
+    const id = parseInt(req.query.limit)
+    if(!isNaN(id) && id>0){
+        res.json(posts.slice(0,id));
+    }else{
+        res.json(posts)
+    }
+ })
+ app.get('/api/users/:id',(req,res)=>{
+    console.log(req.params.id)
+    
     res.json(users)
  })
+
 app.listen(PORT,()=>console.log(`Server is Running on ${PORT}`))
