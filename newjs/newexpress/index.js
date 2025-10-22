edd75ab3662b000a005e9355b50bd047a5384ddb
do4best657 <panic_4_what@hotmail.com>
2025-10-22 12:14:54 +0500

    Tried to train with express router system

--- newjs/newexpress/index.js
+++ newjs/newexpress/index.js
@@ -1,42 +1,8 @@
 import express from 'express'
-import path from 'path'
-import  url  from 'url'
-const __fileName = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__fileName)
-const users = [
-    {id:1,name:"do4best"},
-    {id:2,name:"do4best2"},
-    {id:3,name:"do4best3"},
-
-]
-const posts = [
-    {id:1,name:"do4best"},
-    {id:2,name:"do4best2"},
-    {id:3,name:"do4best3"},
-
-]
+import posts from './routes/routes.js'
 const PORT = process.env.PORT ||3000
+
 const app = express()
-app.get('/',(req,res)=>{
-res.sendFile(path.join(__dirname,'public','home.html'))
-})
-app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'))
-})
-
-
- app.get('/api/posts/',(req,res)=>{
-    const id = parseInt(req.query.limit)
-    if(!isNaN(id) && id>0){
-        res.json(posts.slice(0,id));
-    }else{
-        res.json(posts)
-    }
- })
- app.get('/api/users/:id',(req,res)=>{
-    console.log(req.params.id)
-    
-    res.json(users)
- })
-
+app.use('/api/users/',posts)
+app.use('',posts)
 app.listen(PORT,()=>console.log(`Server is Running on ${PORT}`))
---
+++ newjs/newexpress/routes/routes.js
@@ -0,0 +1,48 @@
+import express from 'express'
+const route = express.Router();
+import path from 'path'
+import  url  from 'url'
+const __fileName = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
+const users = [
+    {id:1,name:"do4best"},
+    {id:2,name:"do4best2"},
+    {id:3,name:"do4best3"},
+
+]
+const posts = [
+    {id:1,name:"do4best"},
+    {id:2,name:"do4best2"},
+    {id:3,name:"do4best3"},
+
+]
+
+route.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,'../public','home.html'))
+})
+route.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public','about.html'))
+})
+
+
+ route.get('/',(req,res)=>{
+    const id = parseInt(req.query.limit)
+    if(!isNaN(id) && id>0){
+        res.json(posts.slice(0,id));
+    }else{
+        res.json(posts)
+    }
+    
+ })
+ route.get('/:id',(req,res)=>{
+    let id = parseInt(req.params.id)
+    const post = posts.find((post)=>post.id === id)
+
+    if(!post){
+        res.status(404).json({error:`Sorry ${post} is not found`})
+    }else{
+        res.status(200).json(posts)
+    }
+ })
+
+ export default route;
