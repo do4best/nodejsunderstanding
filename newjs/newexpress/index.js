5a0177d1ce05dda791e6483a68e10cb029aa5be0
do4best657 <panic_4_what@hotmail.com>
2025-10-24 17:24:36 +0500

    Some Middlewares were exercised such as logger

--- newjs/newexpress/index.js
+++ newjs/newexpress/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import users from './routes/routes.js'
+import logger from './middleware/logger.js'
 
 const PORT = process.env.PORT ||3000
 
 const app = express()
-// app.use(express.json())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 // app.use('',posts)
-app.use('/api/users',users)
+app.use(logger)
+app.use('/api/users/',users)
+
 
 
 
--- newjs/newexpress/routes/routes.js
+++ newjs/newexpress/routes/routes.js
@@ -4,7 +4,7 @@ import path from 'path'
 import  url  from 'url'
 const __fileName = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__fileName)
-const users = [
+let users = [
     {id:1,title:"do4best"},
     {id:2,title:"do4best2"},
     {id:3,title:"do4best3"},
@@ -22,13 +22,13 @@ const users = [
 
  route.get('/',(req,res)=>{
     const rawLimit = req.query.limit;
-    if(rawLimit !== undefined){
+    
     const limit = parseInt(rawLimit,10)
     if(!isNaN(limit) && limit>0){
       return  res.status(200).json(users.slice(0,limit));
     }else{
-        res.status(200).json(users)
-    }}})
+       return res.status(200).json(users)
+    }})
     
 
  route.get('/:id',(req,res)=>{
@@ -38,37 +38,39 @@ const users = [
 
     if(!post){
         res.status(404).json({error:`Sorry ${post} is not found`})
+    }else{
+       return  res.status(200).json(post)
     }
-       return res.status(200).json(post)
+        
     
  })
-//  route.post('/',(req,res)=>{
-// const newPost = {
-//     id:users.length + 1,
-//     title:req.body.title
-// };
-// if(!newPost.title){
-//     return res.status(400).json({msg:"Please include the title"})
-// }
-// users.push(newPost)
-//     res.status(201).json(users)
-//  })
-//  route.put('/:id',(req,res)=>{
-// const id = parseInt(req.params.id)
-// const post = posts.find((post)=>post.id === id)
-// if(!post){
-//     return res.status(404).json({msg:`a user with the ${id} is not found`})
-// }
-// post.title = req.body.title;
-// res.status(200).json(posts)
-//  })
-// route.delete('/:id',(req,res)=>{
-// const id = parseInt(req.params.id)
-// let post = posts.find((post)=>post.id === id)
-// if(!post){
-//     return res.status(404).json({msg:`a user with the ${id} is not found`})
-// }
-// posts = posts.filter((post)=>post.id !== id)
-// res.status(200).json(posts)
-//  })
+ route.post('/',(req,res)=>{
+const newPost = {
+    id:users.length + 1,
+    title:req.body.title
+};
+if(!newPost.title){
+    return res.status(400).json({msg:"Please include the title"})
+}
+users.push(newPost)
+    res.status(201).json(users)
+ })
+ route.put('/:id',(req,res)=>{
+const id = parseInt(req.params.id)
+const post = users.find((post)=>post.id === id)
+if(!post){
+    return res.status(404).json({msg:`a user with the ${id} is not found`})
+}
+post.title = req.body.title;
+res.status(200).json(users)
+ })
+route.delete('/:id',(req,res)=>{
+let id = parseInt(req.params.id)
+let post = users.find((post)=>post.id === id)
+if(!post){
+    return res.status(404).json({msg:`a user with the ${id} is not found`})
+}
+users = users.filter((post)=>post.id !== id)
+res.status(200).json(users)
+ })
  export default route;
