851d3afdf20fcb45a40e7ec0bad346864da2016d
do4best657 <panic_4_what@hotmail.com>
2025-11-11 19:15:38 +0500

    Rebuilding the app with precision

---
+++ jsprojects/brandnewApp/newApp.js
@@ -0,0 +1,21 @@
+const btnEle = document.getElementById("btn")
+function createElement(id,content){
+    const element = document.createElement("textarea")
+    element.classList.add("note");
+    element.placeholder="Empty Note";
+    element.value = content;
+    element.addEventListener("dbclick",()=>{
+        const warning = confirm("Do you want to delete it ?")
+        if(warning){
+            deleteNote(id,element)
+        }
+    })
+}
+function addNote(){
+    const noteObj={
+        id:Math.floor(Math.random()*10000),
+        content:""
+    };
+const neteElement = createElement(noteObj.id,noteObj.content)
+}
+btnEle.addEventListener("click",addNote)
